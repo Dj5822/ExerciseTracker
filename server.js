c60062e64ac62c5a93de9f72e2811b8c49c8630d
dj5822 <Dj5822@gmail.com>
2021-11-09 11:38:54 +1300

    User creation working

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+const express = require('express')
+const app = express()
 require('dotenv').config()
 
 app.use(cors())
@@ -10,6 +11,22 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+/*
+Connect to the database.
+*/
+const mySecret = "mongodb+srv://dj5822:xjE8yXCkex1da5Ih@cluster0.cdewt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mySecret, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+/*
+Create user model.
+*/
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true}
+});
+const User = mongoose.model("User", userSchema);
 
 /*
 Used to load the starting webpage.
@@ -25,17 +42,35 @@ app.post('/api/users', (req, res) => {
   /*
   Should add the username and id to the database.
   */
+  var user = new User({ username: req.body.username });
+  user.save();
 
   res.json({
-    "username": req.body.username,
-    "_id": "test"
+    "username": user.username,
+    "_id": user._id
   });
 });
 
 /*
 Used to get a list of all the users.
 */
+app.get('/api/users', (req, res) => {
+  /*
+  Should get usernames and ids from the database.
+  */
 
+  res.json({
+    "username": "test username",
+    "_id": "test id"
+  });
+});
+
+/*
+Used to post a new exercise.
+*/
+app.post('/api/users/:_id/exercises', (req, res) => {
+  console.log(req.body);
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
