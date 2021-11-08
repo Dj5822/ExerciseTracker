d4d4d58b1e03ef4d36e72badf8d50c8090ef3065
dj5822 <Dj5822@gmail.com>
2021-11-09 10:33:41 +1300

    Added create new user post method.

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+/*
+Used to load the starting webpage.
+*/
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+/*
+Used to create a new user.
+*/
+app.post('/api/users', (req, res) => {
+  /*
+  Should add the username and id to the database.
+  */
 
+  res.json({
+    "username": req.body.username,
+    "_id": "test"
+  });
+});
+
+/*
+Used to get a list of all the users.
+*/
 
 
 
