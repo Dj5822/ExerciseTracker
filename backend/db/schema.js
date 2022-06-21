17b5339c05b448f779fe6a512236201da74e509b
Dylan Jung <Dj5822@gmail.com>
2022-06-21 21:09:04 +1200

    Update schema

--- backend/db/schema.js
+++ backend/db/schema.js
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 
 /*
-Create user model.
+User
+username - the identifier for the user.
 */
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
 });
 
 /*
-  Create exercise model.
-  */
+Exercise
+userId - the userId of the user that did the exercise.
+name - the name of the exercise.
+quantity - the amount of the exercise that was done.
+date - the date the exercise was done.
+*/
 const exerciseSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: Date,
+  name: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  date: { type: Date, required: true},
 });
 
 export const User = mongoose.model("User", userSchema);
