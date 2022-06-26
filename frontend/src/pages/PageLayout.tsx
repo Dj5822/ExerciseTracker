017417449972d62c8fdf5c041cfde48ee46fc67a
Dylan Jung <Dj5822@gmail.com>
2022-06-26 21:09:27 +1200

    Change AppBar text

--- frontend/src/pages/PageLayout.tsx
+++ frontend/src/pages/PageLayout.tsx
@@ -47,7 +47,7 @@ export default function PageLayout() {
                     noWrap
                     sx={{ flexGrow: 1 }}
                 >
-                    Home
+                    Exercise Tracker
                 </Typography>
                 </Toolbar>
             </AppBar>
