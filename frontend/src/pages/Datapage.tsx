c228c8d9283273c732b8c0ba5d0d0dd7aaeba8c3
Dylan Jung <Dj5822@gmail.com>
2022-06-29 00:23:25 +1200

    Show total and highscore

--- frontend/src/pages/Datapage.tsx
+++ frontend/src/pages/Datapage.tsx
@@ -1,7 +1,5 @@
-import { Card, Container } from "@mui/material";
+import { Card, Container, Typography } from "@mui/material";
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
-import AveragescorePanel from "../components/AveragescorePanel";
-import HighscorePanel from "../components/HighscorePanel";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContextProvider";
 
@@ -20,29 +18,37 @@ const Datapage = () => {
         return output;
     });
 
-    return (
-        <ResponsiveContainer width="95%" height="95%">
-            <LineChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                top: 100,
-                right: 0,
-                left: 20,
-                bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line connectNulls type="monotone" dataKey="Push ups" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line connectNulls type="monotone" dataKey="Pull ups" stroke="#82ca9d" />
-            </LineChart>
-        </ResponsiveContainer>
-    );
+    return (<div>
+        <Container sx={{display: "flex", flexDirection: "row", mt: 12}}>
+            {exerciseData.totals.map((exercise:any) => (<Card sx={{p: 2, m: 2, display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Typography variant="h5">{exercise.id}</Typography>
+                <Typography variant="h3">{exercise.total}</Typography>
+                <Typography variant="subtitle1">Total</Typography>
+                <Typography variant="h3">{exercise.highscore}</Typography>
+                <Typography variant="subtitle1">Highscore</Typography>
+            </Card>))}
+            
+        </Container>
+        <LineChart
+            width={1750}
+            height={600}
+            data={data}
+            margin={{
+            top: 100,
+            right: 0,
+            left: 0,
+            bottom: 5,
+            }}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line connectNulls type="monotone" dataKey="Push ups" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line connectNulls type="monotone" dataKey="Pull ups" stroke="#82ca9d" />
+        </LineChart>
+    </div>);
 }
 
 export default Datapage;
